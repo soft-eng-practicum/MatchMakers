3592442593982b72c86d7d8830b3003f31d13e5a
lmarsicano <lmarsicano@ggc.edu>
2018-09-27 20:20:12 -0400

    add PASchools.ts with attributes and constructor

---
+++ src/PASchools.ts
@@ -0,0 +1,15 @@
+class PASchools
+{
+  name: String;
+  state: String;
+  minCumulativeGPA: double;
+  minScienceGPA: double;
+  minRequiredHCE: int;
+  GRERequired: boolean;
+
+  constructor(name: String, state: String, minCumulativeGPA: double, minScienceGPA: double,
+                minRequiredHCE: int, GRERequired: boolean)
+  {
+
+  }
+}
