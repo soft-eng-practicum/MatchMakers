ea63ea7745f2066e01cf331441f490d66184d0ff
lmarsicano <lmarsicano@ggc.edu>
2018-10-02 23:17:56 -0400

    chang schools.ts to pa-schools.app.component.ts, fix compile errors, change angular logo to paPicture.png

---
+++ src/app/pa-schools.component.ts
@@ -0,0 +1,17 @@
+class PASchools
+{
+  name: string;
+  state: string;
+  minCumulativeGPA: number;
+  minScienceGPA: number;
+  minRequiredHCE: number;
+  GRERequired: boolean;
+
+  
+  // constructor(String name, String state, double minCumulativeGPA, double minScienceGPA,
+  //               int minRequiredHCE, boolean GRERequired)
+  // {
+  //
+  // }
+
+}
