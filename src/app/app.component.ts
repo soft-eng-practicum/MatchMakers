0df72faca12534345dbb5b768a1580735ff10525
lmarsicano <lmarsicano@ggc.edu>
2018-10-13 17:24:11 -0400

    add angular function to read user clicks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+// export class AppComponent
+// {
+//   title = 'PA-Matcher';
+// }
+
 export class AppComponent
 {
-  title = 'PA-Matcher';
+  private dataFile: string = "/assets/data/schools.json";
+
+  selectedState: string = '';
+
+  selectChangeHandler (event: any)
+  {
+    this.selectedState = event.target.value;
+  }
+
 }
