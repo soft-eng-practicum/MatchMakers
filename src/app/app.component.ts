ca328e1ef36eea5511f7edd097be2fc791ac0688
lmarsicano <lmarsicano@ggc.edu>
2018-10-18 18:56:04 -0400

    add constructor to create school array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,35 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-// export class AppComponent
-// {
-//   title = 'PA-Matcher';
-// }
 
 export class AppComponent
 {
-  private dataFile: string = '/assets/data/schools.json';
+  title = 'Schools';
+  constructor (private httpService: HttpClient) { }
   private selectedState: string = '';
+  private schoolList: string = '';
+  //
+  // ngOnInit()
+  // {
+  //   this.httpService.get("./assets/data/schools.json").subscribe(
+  //     data => {
+  //       this.arrSchools = data as string [];
+  //     }
+  //   )
+  // }
 
   selectChangeHandler (event: any)
   {
     this.selectedState = event.target.value;
+    this.schoolList = this.httpService.get("./assets/data/schools.json").subscribe(
+      data => {
+        this.arrSchools = data as string [];
+      }
   }
 
 }
