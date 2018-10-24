88de4657bc66eb1d2ffd8ffc2f5344da1f2aa1c6
BreeDay <bday@ggc.edu>
2018-10-24 12:23:34 -0400

    J/K This is the actual working JSON file for the dropdown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent
     this.selectedState = event.target.value;
     var obserschoolList = this.httpService.get("./assets/data/schools.json");
 
-   obserschoolList.subscribe((data: PASchools) => this.schoolList = data.filter(item => item.state == this.selectedState); );
+   obserschoolList.subscribe((data: PASchools) => this.schoolList = data.filter(item => item.state == this.selectedState) );
    console.log(this.schoolList);
   }
 
