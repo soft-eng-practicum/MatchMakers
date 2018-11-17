a6735dd613b8c4355fa93131d447f53040684418
lmarsicano <lmarsicano@ggc.edu>
2018-11-16 23:13:30 -0500

    add in user rating calculation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,26 @@ export class AppComponent
 {
   title = 'Schools';
   showStudentInfo = true;
-  showSchools = false;
   constructor (private httpService: HttpClient) { }
   private selectedState: string = '';
   private schoolList: Array<PASchools> = [];
 
-  selectChangeHandler (event: any)
+  studentCumuGPA: number = 0;
+  studentSciGPA: number = 0;
+  studentGREVerbal: number = 0;
+  studentGREQuant: number = 0;
+  studentGREEssay: number = 0;
+  studentHCE: number 0;
+
+  schoolCumuGPA: number = 0;
+  schoolSciGPA: number = 0;
+  schoolHCE: number = 0;
+  schoolGRE: boolean = false;
+
+  score: number = 0;
+
+
+  selectSchool(event: any)
   {
     this.selectedState = event.target.value;
     var obserschoolList = this.httpService.get("./assets/data/schools.json");
@@ -28,14 +42,68 @@ export class AppComponent
   submitStudentInfo(studentInfo: any)
   {
     this.showStudentInfo = false;
-    this.showSchools = true;
     console.log("pressed: " + studentInfo.cumuGPA + " " + studentInfo.sciGPA +  " " + studentInfo.greVerbal +
                     " " + studentInfo.greQuant + " " + studentInfo.greEssay +  " " + studentInfo.hce);
+
+    this.studentCumuGPA = studentInfo.cumuGPA;
+    this.studentSciGPA = studentInfo.sciGPA;
+    this.studentGREVerbal = studentInfo.greVerbal;
+    this.studentGREQuant = studentInfo.greQuant;
+    this.studentGREEssay = studentInfo.greEssay;
+    this.studentHCE = studentInfo.hce;
   }
 
   storeSchool(schoolID: number)
   {
     console.log(this.schoolList[schoolID].name + " " + this.schoolList[schoolID].minCumulativeGPA);
+
+    this.schoolCumuGPA = this.schoolList[schoolID].minCumulativeGPA;
+    this.schoolSciGPA = this.schoolList[schoolID].minScienceGPA;
+    this.schoolHCE = this.schoolList[schoolID].minRequiredHCE;
+    this.schoolGRE = this.schoolList[schoolID].GRERequired;
+  }
+
+  calculateRating()
+  {
+    console.log("calculate");
+    this.score = 0;
+    this.rating = "";
+    if (this.studentCumuGPA >= this.schoolCumuGPA)
+    {
+      this.score++;
+    }
+    if (this.studentSciGPA >= this.schoolSciGPA)
+    {
+      this.score++;
+    }
+    if (this.studentHCE >= this.schoolHCE)
+    {
+      this.score++;
+    }
+    if (this.schoolGRE == true)
+    {
+      if (this.studentGREQuant > 0 && this.studentGREVerbal > 0 && this.studentGREEssay > 0)
+      {
+        this.score += 3;
+      }
+    }
+    if (this.schoolGRE == false)
+    {
+      this.score += 3;
+    }
+
+    if (this.score <= 2)
+    {
+      this.rating = "Poor";
+    }
+    else if (this.score <=4)
+    {
+      this.rating = "Average";
+    }
+    else
+    {
+      this.rating = "Good";
+    }
   }
 
 }
